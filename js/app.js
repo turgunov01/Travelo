8702f26c88815fa720fff5aef51199902521a19b
Ваше Имя <you@example.com>
2023-05-11 18:02:43 +0500

    responsive & no nav

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
-const parallax = document.querySelectorAll('.parallax')
 
-let xValue = 0
-let yValue = 0
+document.addEventListener('DOMContentLoaded', () => {
+    let xValue = 0
+    let yValue = 0
 
-for (let i = 0; i < parallax.length; i++) {
-    parallax[i].style.transform = `translateX(calc(-50% + ${-xValue}px)) translateY(calc(-50% + ${yValue}px))`
-}
+    function getCurrentClientPosition(e) {
+        xValue = e.clientX
+        yValue = e.clientY
+        console.log(xValue, yValue)
+    }
 
-window.addEventListener('mousemove', (e) => {
-    xValue = e.clientX - window.innerWidth / 2
-    yValue = e.clientY - window.innerHeight / 2
-
+    const parallax = document.querySelectorAll('.parallax')
 
     for (let i = 0; i < parallax.length; i++) {
-        let speedx = parallax[i].dataset.speedx;
-        let speedy = parallax[i].dataset.speedy;
-        parallax[i].style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`
+        parallax[i].style.transform = `translateX(calc(-50% + ${-xValue}px)) translateY(calc(-50% + ${yValue}px))`
     }
-})
+    getCurrentClientPosition(event)
 
+
+    window.addEventListener('mousemove', (e) => {
+        let once = 0
+        xValue = e.clientX - window.innerWidth / 2
+        yValue = e.clientY - window.innerHeight / 2
+
+
+        for (let i = 0; i < parallax.length; i++) {
+            let speedx = parallax[i].dataset.speedx;
+            let speedy = parallax[i].dataset.speedy;
+            parallax[i].style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`
+        }
+    })
+
+
+})
